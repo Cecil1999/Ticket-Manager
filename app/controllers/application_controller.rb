4368476d62d37a29cd2932d0a3ca62275f27ae8d
Cecil1999 <Cecil2090@gmail.com>
2025-06-17 19:36:07 -0400

    Adding Admin portal path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   before_action :is_loggedin
+  helper_method :is_admin?
 
   def is_loggedin
     unless verify_session?
@@ -10,6 +11,10 @@ class ApplicationController < ActionController::Base
 
   def verify_session?
     !!session[:username]
+  end
+
+  def is_admin?
+    return User.find_by(username: session[:username]).admin?
   end
 
   allow_browser versions: :modern
