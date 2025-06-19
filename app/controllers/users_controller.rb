e69b603df24e140890fc96a7027ac76fa1ab774b
Cecil1999 <Cecil2090@gmail.com>
2025-06-18 20:58:36 -0400

    Add Create logic + show page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[ show edit update destory ]
+
   def index
   end
 
@@ -7,15 +9,36 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: "User was successfully created." }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
   end
 
   def destory
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.expect(user: [ :username, :password ])
