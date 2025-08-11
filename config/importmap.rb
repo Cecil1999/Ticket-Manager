9674f714368596ba36eea907d846c1f88362cca6
Cecil1999 <Cecil2090@gmail.com>
2025-08-11 19:22:56 -0400

    Fix popper/core import. Use URL

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,4 +2,8 @@
 
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "bootstrap" # @5.3.7pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
+pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.8/dist/esm/index.js" # @2.11.8
+pin "bootstrap", to: "bootstrap.bundle.min.js"
+pin "application", to: "application.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
