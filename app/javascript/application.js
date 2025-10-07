a8c9a695b5e231f4849b27ac59836e8b3357e5ed
Cecil1999 <Cecil2090@gmail.com>
2025-10-06 21:08:45 -0400

    Update edit path.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,3 +4,12 @@ import "controllers"
 import "@popperjs/core"
 import "bootstrap"
 import * as bootstrap from "bootstrap"
+
+const $change_password_cb = document.getElementById('change_password_cb');
+$change_password_cb.addEventListener('change', () => {
+    if ($change_password_cb.checked) {
+        document.getElementById('change_password_div').classList.remove('d-none');
+    } else {
+        document.getElementById('change_password_div').classList.add('d-none');
+    }
+});
